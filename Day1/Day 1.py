a7f584c62a7b588225c902adc49a35aad78fec89
kyhomelab <151972930+kyhomelab@users.noreply.github.com>
2023-12-06 18:53:08 -0500

    First few basic concepts for the day

---
+++ Day1/Day 1.py
@@ -0,0 +1,13 @@
+print("Hello World!")
+print("First lines")
+# First Comment
+
+print("""Doing a multiline
+string. Seeing the 
+limits of this.
+Okay so it allows me to do a lot of lines, NEAT""")
+
+print("Doing a new line indicator \n" "uuuuhhh test")
+
+# Now using \n and * to multiply the print
+print("Testing \n" * 3)
