9aca6f8b670cc845ea1596c7f20233bf92274aea
kyhomelab <151972930+kyhomelab@users.noreply.github.com>
2023-12-06 20:26:44 -0500

    First few basic concepts for the day

---
+++ Day1/Input Function.py
@@ -0,0 +1,14 @@
+# print("Hey Welcome to KyHomelab")
+# print(input("What is your name?"))
+
+# name = "Kyle"
+# print(name)
+# name = "Dog"
+# print(name)
+
+print("Hey Welcome to KyHomelab")
+name = input("What is your name?\n")
+print("Hello " + name + ", how are you doing today?")
+feeling = input()
+
+print("Im glad you are feeling " + feeling)
