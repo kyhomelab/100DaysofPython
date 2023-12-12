03cd0ef820648cbb6d97b5c789f5c527d15044ef
kyhomelab <151972930+kyhomelab@users.noreply.github.com>
2023-12-12 13:29:52 -0500

    Trying a small project on my own

---
+++ Day4/3. Rectangle Area.py
@@ -0,0 +1,6 @@
+width = input("Welcome to Area of a Rectangle Formula. What is the width?\n")
+length = input('What is the legnth?\n')
+
+area = int(width) * int(length)
+
+print("The total area = ",area)
