6334c5721f8844b850d706de3c484cadd43d80a2
kyhomelab <151972930+kyhomelab@users.noreply.github.com>
2023-12-12 13:29:12 -0500

    Learning about using Python Lists

---
+++ Day4/2. Remove from List.py
@@ -0,0 +1,15 @@
+supplies = ["tent", "sleeping bags", "water", "rasberry pi", "knife", "fishing rod", "lighter", "marshmallows"]
+
+site = ["Crystal Lake", 404, 89.3, True]
+
+supplies.extend(["toilet paper", "bidet"])
+# supplies.clear()
+# supplies.remove("water")
+# supplies.remove("tent")
+
+# supplies.pop(0)
+print(supplies.pop(0))
+supplies.pop(0)
+
+print(supplies)
+
