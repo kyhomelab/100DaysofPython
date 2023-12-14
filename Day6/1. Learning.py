9eac222b95fed382b9ea4ef703fc1527c99856ac
kyhomelab <151972930+kyhomelab@users.noreply.github.com>
2023-12-14 15:28:28 -0500

    Learning More

---
+++ Day6/1. Learning.py
@@ -0,0 +1,14 @@
+# fruits = ["apple", "banana", "cherry"]
+#
+# for fruit in fruits:
+#     print(fruit + " pie")
+
+# for num in range (1,100):
+#     print(num)
+
+# ___________________
+
+# Find all numbers divisible by 3 numbers from 1-100
+for num in range (0,100):
+    if num % 3 == 0:
+        print(num)
